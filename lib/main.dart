16b9aef2bf460a27a33395fb8bd3d8734f599486
Newton <nkimathi@MacBook-Pro-van-Newton.local>
2023-06-23 09:15:14 +0300

    using color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:expense_trucker/widgets/expenses.dart';
 
+final kcolorScheme = ColorScheme.fromSeed(
+        seedColor:const  Color.fromARGB(
+          159, 82, 78, 140
+          ),
+      );
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData().copyWith(useMaterial3: true, scaffoldBackgroundColor: Color.fromARGB(159, 82, 78, 140)),
-      home:const Expenses(),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kcolorScheme,
+        appBarTheme:const AppBarTheme().copyWith(
+          backgroundColor: kcolorScheme.onPrimaryContainer,
+          foregroundColor: kcolorScheme.primaryContainer
+        )
+      ),
+      home: const Expenses(),
     ),
   );
 }
