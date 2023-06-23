b3c63a56cc60a493a5ab4bca89dfd069938165f8
Newton <nkimathi@MacBook-Pro-van-Newton.local>
2023-06-23 09:37:00 +0300

    theme data in widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:expense_trucker/widgets/expenses.dart';
 
 final kcolorScheme = ColorScheme.fromSeed(
-        seedColor:const  Color.fromARGB(
-          159, 82, 78, 140
-          ),
-      );
+  seedColor: const Color.fromARGB(159, 82, 78, 140),
+);
 
 void main() {
   runApp(
@@ -13,9 +11,23 @@ void main() {
       theme: ThemeData().copyWith(
         useMaterial3: true,
         colorScheme: kcolorScheme,
-        appBarTheme:const AppBarTheme().copyWith(
-          backgroundColor: kcolorScheme.onPrimaryContainer,
-          foregroundColor: kcolorScheme.primaryContainer
+        appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kcolorScheme.onPrimaryContainer,
+            foregroundColor: kcolorScheme.primaryContainer),
+        cardTheme: const CardTheme().copyWith(
+            color: kcolorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kcolorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: kcolorScheme.onSecondaryContainer,
+            fontSize: 14,
+          )
         )
       ),
       home: const Expenses(),
